1dd60425378aeb1ed44fabc6f774b038a99d5b63
LenweSaralonde <lenwe@lenwe.info>
2020-06-21 14:56:09 +0200

    Simplified ergonomy, added in-game help

--- FakeAchievement.lua
+++ FakeAchievement.lua
@@ -6,48 +6,128 @@
 	Author:  @project-author@
 ]]
 
-SlashCmdList["FAKEACHIEVEMENT"] = function(s)
+--- Display a message in the console
+-- @param msg (string)
+function FakeAchievement_Print(msg)
+	DEFAULT_CHAT_FRAME:AddMessage(msg, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
+end
 
-	local day, month, year, aLink, name, id
-	local a, b, c, d, e
+--- Display an error message in the console
+-- @param msg (string)
+function FakeAchievement_Error(msg)
+	DEFAULT_CHAT_FRAME:AddMessage(msg, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b)
+	PlaySound(32051)
+end
 
-	local myGuid = UnitGUID("target")
-	local myName = UnitName("target")
+--- Get achievement link
+-- @param name (string) Achievement name
+-- @param id (string) Achievement ID
+-- @param guid (string) Player GUID
+-- @param day (int) Day
+-- @param month (int) Month
+-- @param year (int) Year
+-- @return link (string)
+function FakeAchievement_GetLink(name, id, guid, day, month, year)
+	id = tonumber(id, 10)
+	day = tonumber(day, 10)
+	month = tonumber(month, 10)
+	year = tonumber(year, 10) % 100
 
-	if (myGuid == nil) or (myGuid == "") then
-		myGuid = UnitGUID("player")
-		myName = UnitName("player")
+	return "|cffffff00|Hachievement:"..id..":"..guid..":1:"..month..":"..day..":"..year..":4294967295:4294967295:4294967295:4294967295|h["..name.."]|h|r"
+end
+
+--- Extract achievement ID and name from link
+-- @param link (string) Achievement link or ID
+-- @return id (string) Achievement ID
+-- @return name (string) Achievement name
+function FakeAchievement_ExtractAchievement(link)
+	-- Extract achievement link from string
+	-- |cffffff00|Hachievement:1789:Player-3714-06447380:0:0:0:-1:0:0:0:0|h[Corvées journalières]|h|r,
+	local id, name
+	local regexp = "|cffffff00|Hachievement:([0-9]+):(.+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+)|h%[([^]]+)%]|h|r"
+	for id, _, _, _, _, _, _, _, _, _, name in string.gmatch(link, regexp) do
+		return id, name
 	end
 
-	myGuid = string.gsub(myGuid, '0x', '')
+	-- Link not found: search by ID
+	id = tonumber(link, 10)
+	_, name = GetAchievementInfo(id)
+	if name ~= nil then
+		return id, name
+	end
 
-	for id, day, month, year in string.gmatch(s, "([0-9]+)%s+([0-9]+)/([0-9]+)/([0-9]+)") do
-		_, name = GetAchievementInfo(id)
-		if (name == nil) then
+	-- Invalid achievement
+	return nil, nil
+end
+
+--- Display help
+--
+function FakeAchievement_Help()
+	FakeAchievement_Print("FakeAchievement usage:")
+	FakeAchievement_Print("|cFFFFFFFF/fa <achievement link or ID> <day>/<month>/<year>|r")
+	FakeAchievement_Print("|cFFFFFFFF<achievement link or ID>|r: Achievement link (Shift+click on achievement from achievement list) or achievement ID (from WoWHead URL).")
+	FakeAchievement_Print("|cFFFFFFFF<day>|r/|cFFFFFFFF<month>|r/|cFFFFFFFF<year>|r: Achievement date.")
+
+	local id, name = FakeAchievement_ExtractAchievement(14068)
+	local example1 = "|cFFFFFFFF/fa 14068 15/4/2020|r"
+	local example2 = "|cFFFFFFFF/fa|r |cffffff00|Hachievement:14068:" .. string.gsub(UnitGUID('player'), '0x', '') .. ":0:0:0:-1:0:0:0:0|h[" .. name .. "]|h|r |cFFFFFFFF15/4/2020|r"
+
+	FakeAchievement_Print("Example: \n" .. example1 .. "\n" .. example2)
+end
+
+--- Main /fa command
+-- Example: /fa 4999 8/12/10
+-- @param s (string)
+SlashCmdList["FAKEACHIEVEMENT"] = function(s)
+	local success = pcall(function()
+		-- Get achievement target
+		local targetGuid = UnitGUID('target')
+		local targetName = UnitName('target')
+
+		if targetGuid == nil or targetGuid == "" then
+			targetGuid = UnitGUID('player')
+			targetName = UnitName('player')
+		elseif not(UnitIsPlayer("target")) then
+			FakeAchievement_Error("The targeted unit |cFFFFFFFF" .. targetName .. "|r is not a player.")
 			return
 		end
 
-		DEFAULT_CHAT_FRAME:AddMessage("Achievement for "..myName..": |cffffff00|Hachievement:"..id..":"..myGuid..":1:"..month..":"..day..":"..year..":4294967295:4294967295:4294967295:4294967295|h["..name.."]|h|r")
-		return
-	end
+		targetGuid = string.gsub(targetGuid, '0x', '')
 
-	for a, b, c, d in string.gmatch(s, "(.+)%s+([0-9]+)/([0-9]+)/([0-9]+)") do
-		aLink = a
-		day   = b
-		month = c
-		year  = d
-	end
+		-- Extract fake achievement parameters
+		local day, month, year, link
+		local a, b, c, d, e
 
-	if (aLink == nil) then
-		return
-	end
+		for a, b, c, d in string.gmatch(s, "(.+)%s+([0-9]+)/([0-9]+)/([0-9]+)") do
+			link = a
+			day   = b
+			month = c
+			year  = d
+		end
 
-	local aId, playerGuid, aDone, aMonth, aDay, aYear, aName
-	local regexp = "|cffffff00|Hachievement:([0-9]+):(.+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+):([%-0-9]+)|h%[([^]]+)%]|h|r"
-	-- |cffffff00|Hachievement:1789:Player-3714-06447380:0:0:0:-1:0:0:0:0|h[Corvées journalières]|h|r,
+		-- Invalid parameters provided: display help
+		if not(link) or not(day) or not(month) or not(year) then
+			FakeAchievement_Help()
+			return
+		end
 
-	for aId, playerGuid, aDone, aMonth, aDay, aYear, _, _, _, _, aName in string.gmatch(aLink, regexp) do
-		DEFAULT_CHAT_FRAME:AddMessage("Achievement for "..myName..": |cffffff00|Hachievement:"..aId..":"..myGuid..":1:"..month..":"..day..":"..year..":4294967295:4294967295:4294967295:4294967295|h["..aName.."]|h|r")
+		-- Extract achievement name and ID
+		local id, name = FakeAchievement_ExtractAchievement(link)
+
+		-- Achievement not found
+		if not(id) then
+			FakeAchievement_Error("Invalid achievement ID or link.")
+			return
+		end
+
+		-- Display faked achievement link in the console
+		local playerLink = "|cFFFFFFFF|Hplayer:" .. targetName .. "|h" .. targetName .. "|h|r"
+		local achievementLink = FakeAchievement_GetLink(name, id, targetGuid, day, month, year)
+		FakeAchievement_Print("Achievement for " .. playerLink .. ": " .. achievementLink)
+	end)
+
+	if not(success) then
+		FakeAchievement_Help()
 	end
 end
 
--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 Fake Achievement
 ================
-Create fake achievement / curve links to be used in chat messages to impress noobs, fake PGMs and make laugh your friends and the girls ! Also helpful to join a pickup group when requested to "link curve plz" so you can make it for real.
+Create fake achievement / curve links for chat messages to impress noobs, fake PGMs and make laugh your friends and the girls!
+
+Also helpful to join a pickup group when requested to "link curve plz" so you can make it for real.
+
+Of course, this won't give you the achievement for real and the actual status can be verified in the armory by anyone.
 
 Usage
 -----
+Target the player you want to generate the fake achievement for then type the following command:
+
 `/fa <achievement> <date>`
-* Achievement can be the achievement ID (retrieved from the WoWHead URL for instance) or any incomplete achievement link.
-* Date is format is *DD*/*MM*/*YY*. Year is in 2-digit format representing the year of the 21st Century (ie 8/12/10 for the 8<sup>th</sup> december 2010)
+* **Achievement** can be the achievement ID (retrieved from the WoWHead URL for instance) or any incomplete achievement link (shift + click from the achievement list).
+* **Date** format is *DD*/*MM*/*YYYY* or *DD*/*MM*/*YY*. (ie 8/12/2010 for the 8<sup>th</sup> december 2010)
+
+The achievement link is generated for the currently targeted player. If you don't have any target, the link is generated for your character.
 
 Examples
 --------
-`/fa 4999 8/12/10`
-`/fa [Realm First! Level 85] 8/12/10`
+`/fa 4999 8/12/2010`
 
-The achievement link is generated for the currently targeted player. If you don't have any target, the link is generated for your character. Gives spoofy results with NPCs.
-
-Of course, this won't give you the achievement and the real achievement can be verified in the armory. This is just attended for fun and to shut those arrogant fake PGM's mouths.
+`/fa [Realm First! Level 85] 8/12/2010`
